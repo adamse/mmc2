d876a5093eee87084a0aa6cc3c61284aeff616c4
Jacob Jonsson <jassob@live.se>
2014-12-07 10:49:08 +0100

    Rewrote the dijkstra code and refactored it into separate functions to make it more readable

--- src/Agent/Dijkstra.hs
+++ src/Agent/Dijkstra.hs
@@ -2,9 +2,11 @@
 -- |
 -- An agent that implements Dijksta's 
 -- algorithm to find shortest path
-module Agent.Dijkstra (
+module Agent.Dijkstra {-(
   DijkstraAgent() 
-  ) where
+  )-} where
+
+import Debug.Trace
 
 import Control.Monad.State.Strict
 import Data.Foldable as F
@@ -35,11 +37,8 @@ instance Agent DijkstraAgent where
   stepAgent fs@(FromServer {..}) =
     if ((Carryable Banana) `Prelude.elem` inventory)
         then return (Use Banana)
-        else
-            if length inventory < inventorySize
-            then goValuable fs
-            else goUser fs
-
+        else goToTarget fs 
+                      
 goValuable :: FromServer -> StateT DijkstraAgent IO Command
 goValuable (FromServer {..}) = do
     liftIO $ print "Valuable"
@@ -63,6 +62,42 @@ goUser (FromServer {..}) = do
     pl = positionedLayout layout
     movv t = user t || movable t
 
+goToTarget :: FromServer -> StateT DijkstraAgent IO Command
+goToTarget fs@(FromServer {..}) = do
+    if inventoryFull
+      then  liftIO $ print "Users"
+      else  liftIO $ print "Valuables and carryables"
+    let (dm, pm) = dijkstra position pl $
+                   neighbours (getFiltering inventoryFull) pl
+    let target = getTarget dm pl (calcPrio inventoryRatio)
+    let path = reverse $ constructPath pm position target
+    let m1 = move position (path !! 1)
+    if (speedyBuffs fs) > 7
+      then do
+          let m2 = move (path !! 1) (path !! 2)
+          return (Moves [m1,m2])
+      else
+      return (Move m1)
+  where
+    pl = positionedLayout layout
+    inventoryFull = not (length inventory < inventorySize)
+    inventoryRatio = d / i
+      where d = fromIntegral $ length inventory :: Double
+            i = fromIntegral $ inventorySize :: Double
+
+-- | Gets the filter rules for tiles 
+getFiltering :: Bool -- ^ Should we go to user?
+                -> Tile -- ^ Tile to filter
+                -> Bool 
+getFiltering True tile  = user tile || movable tile || carryable tile || valuable tile
+getFiltering False tile = movable tile || carryable tile || valuable tile
+
+-- | Checks whether we have a speedy buff
+speedyBuffs :: FromServer -> Int
+speedyBuffs fs | hasBuffs = fromMaybe 0 (speedy $ fromJust $ buffs fs)
+               | otherwise = 0
+  where hasBuffs = isJust (buffs fs)
+
 getTarget dm pl gn =
   M.foldrWithKey
     (\p _ currentBest ->
@@ -72,7 +107,18 @@ getTarget dm pl gn =
     (0,0)
     dm
 
+-- | Compares the priorities of two positions, p1 and p2, by calling a priority function with both positions as arguments. Returns the ordering of this comparison
+cmpGoodness :: PositionedLayout -- ^ Map of positions and their tiles
+               -> DistanceMap -- ^ Map of positions and their distances from a start node
+               -> (DistanceMap -> PositionedLayout -> Position -> Int) -- ^ Priority function
+               -> Position -- ^ First position
+               -> Position -- ^ Second position
+               -> Ordering -- ^ The result of comparing p1's and p2's priorities
 cmpGoodness pl dm gn p1 p2 = compare (gn dm pl p1) (gn dm pl p2)
+
+selectGoodness :: Bool -> (DistanceMap -> PositionedLayout -> Position -> Int)
+selectGoodness inventoryFull | inventoryFull = userGoodness 
+                             | otherwise = goodness
 
 goodness :: DistanceMap -> PositionedLayout -> Position -> Int
 goodness dm pl p =
@@ -82,12 +128,38 @@ goodness dm pl p =
         Valuable Playlist -> 10
         Valuable Album -> 4
         Valuable Song -> 1
-        Carryable Banana -> 15
+        Carryable Banana -> 4
         _ -> -100000
   in if (valuable t) || (carryable t)
      then d - prio
      else 100000
-     
+
+-- | Calculates the cost for a tile by subtracting the priority 
+--   for that tile from the distance calculated in dijkstra
+calcPrio :: Double -- ^ Length of inventory / inventorySize
+            -> DistanceMap -- ^ Map of positions and their distances
+            -> PositionedLayout -- ^ Map of positions and their tiles
+            -> Position -- ^ Position to calculate
+            -> Int -- ^ The weighted cost of a position
+calcPrio r dm pl p = d - prio
+  where d = trace (show p ++ show (distance dm p)) $ fromJust $ M.lookup p dm
+        t = fromJust $ M.lookup p pl
+        prio = getPrio t r 
+
+-- | Gets the priorities for valuable tiles.
+-- | Higher value means higher priority
+getPrio :: Tile -- ^ The tile to get a value from
+           -> Double -- ^ Ratio of inventory length and inventorysize
+           -> Int -- ^ The prio of the tile
+getPrio (Valuable Playlist) _ = 10
+getPrio (Valuable Album) _ = 6
+getPrio (Valuable Song) _ = 1
+getPrio (Carryable Banana) _ = 8
+getPrio User ratio | ratio > 0.75 = 15
+                   | ratio > 0.5 = 3
+                   | otherwise = (-100000)
+getPrio _ _ = -10000000000
+
 userGoodness :: DistanceMap -> PositionedLayout-> Position -> Int
 userGoodness dm pl p = if t then dist else dist + 100000
   where t = maybe False user (M.lookup p pl)
@@ -111,11 +183,11 @@ constructPath pm p t
 newDistance :: Position -> DistanceMap
 newDistance us = M.singleton us 0
 
-dijkstra :: Position -- ^ Start position
-         -> PositionedLayout
-         -> (Position -> Set Position) -- ^ The graph
+{-dijkstraOld :: Position -- ^ Start position
+         -> PositionedLayout -- ^ Positioned layout
+         -> (Position -> Set Position) -- ^ Neighbour function
          -> (DistanceMap, PrevMap)
-dijkstra start layout graph = go (newDistance start) M.empty (S.insert start $ allnodes layout)
+dijkstraOld start layout graph = go (newDistance start) M.empty (S.insert start $ allnodes layout)
   where
     go d p q =
      if S.null q
@@ -130,9 +202,30 @@ dijkstra start layout graph = go (newDistance start) M.empty (S.insert start $ a
                ) (d, p) $ graph u
 
           in go d' p' q'
+-}
+
+dijkstra :: Position -> PositionedLayout -> (Position -> Set Position) -> (DistanceMap, PrevMap)
+dijkstra start layout graph = dijkstra' (newDistance start) M.empty (S.insert start $ allnodes layout) graph
+
+dijkstra' :: DistanceMap -> PrevMap -> Set Position -> (Position -> Set Position) -> (DistanceMap,PrevMap)
+dijkstra' dist prevs unvisited neigh
+  | S.null unvisited = (dist, prevs)
+  | otherwise = dijkstra' newDists newPrevs nodesLeft neigh
+  where closestNode = F.minimumBy (cmpDist dist) unvisited
+        nodesLeft   = S.delete closestNode unvisited
+        neighbours  = neigh closestNode
+        (newDists, newPrevs) = S.foldr (compareDistance closestNode) (dist,prevs) neighbours
+
+compareDistance :: Position -> Position -> (DistanceMap, PrevMap) -> (DistanceMap, PrevMap)
+compareDistance pos neighbour (d, p)
+  | alt < dist = (M.insert neighbour alt d, M.insert neighbour pos p)
+  | otherwise = (d, p)
+  where alt = distance d pos + 1
+        dist = distance d neighbour
 
 distance :: DistanceMap -> Position -> Int
 distance m p = fromMaybe 100000 (M.lookup p m)
+
 cmpDist :: DistanceMap -> Position -> Position -> Ordering
 cmpDist m p1 p2 = let d1 = distance m p1
                       d2 = distance m p2
@@ -164,3 +257,12 @@ allnodes playout
   = S.fromList
   $ M.foldrWithKey (\k _ acc -> k:acc ) []
   $ M.filter movable playout
+
+----------- Test data  ------------
+
+fsTest = FromServer {layout = [[Empty,Empty,Empty,Empty,Empty,Empty,Empty,Valuable Playlist,Valuable Playlist,Valuable Playlist],[Empty,Carryable Banana,Wall,Wall,Wall,Wall,Wall,Wall,Wall,Wall],[Empty,Empty,Valuable Song,Valuable Song,Empty,Empty,Empty,User,Wall,Valuable Playlist],[Valuable Album,Empty,Empty,Empty,Empty,Wall,Wall,Wall,Valuable Playlist,Valuable Playlist],[Wall,Wall,Carryable Banana,Valuable Album,Empty,Empty,Empty,Valuable Song,Wall,Empty],[Valuable Song,Carryable Banana,Wall,Wall,Wall,Valuable Album,Empty,Valuable Song,Wall,Empty],[Valuable Song,Wall,Valuable Album,Valuable Song,Valuable Song,Wall,Empty,Empty,Carryable Banana,Empty],[Monkey,Wall,Monkey,Empty,Empty,Empty,Empty,Empty,Empty,Empty],[Empty,User,Empty,Empty,Wall,Wall,Empty,Empty,Wall,Wall],[Empty,Empty,Valuable Song,Valuable Song,Carryable Banana,Wall,Empty,Valuable Song,Valuable Song,Valuable Album]], position = (7,0), remainingTurns = 50, isGameOver = False, score = 0, buffs = Just (Buffs {speedy = Nothing, immobilized = Nothing}), inventory = [], inventorySize = 3}
+
+plTest = positionedLayout (layout fsTest)
+iRTest = (fromIntegral (length (inventory fsTest))) / (fromIntegral (inventorySize fsTest))
+dmPmTest = dijkstraOld (position fsTest) plTest (neighbours (getFiltering True) plTest)
+--dmPmTest2 = dijkstra (position fsTest) plTest (neighbours (getFiltering True) plTest)
